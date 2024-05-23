646a8fba9e1eebed3f8c7e51f8c52104eb01d786
Bhuvaneshwar <bhuvaneshwarmarri@gmail.com>
2024-05-23 12:59:26 +0530

    version 0.1.1

---
+++ src/webopen/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+import webbrowser
+import argparse
+from flask_sqlalchemy import SQLAlchemy 
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column 
+from sqlalchemy import Integer, String
